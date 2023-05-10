f40c1740fc7629d412ec880f84aaba7bbfab3f7d
lwafulah <loice.lovie@gmail.com>
2023-05-10 12:24:47 -0700

    Random number

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(number)
+
+if (number == 0):
+    print(number, "is zero")
+elif (number > 0):
+    print(number, "is positive")
+else:
+    print(number, "is negative")
