2cb2cea0f15f6de6e557e17181ce0b0849ed6c4e
lwafulah <loice.lovie@gmail.com>
2023-05-13 11:30:00 -0700

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,24 +8,31 @@ if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
 
-# Get the arguments
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+# Get the arguments and operator
+a, operator, b = sys.argv[1:]
 
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
+# Check if operator is valid
+if operator not in "+-*/":
     print("Unknown operator. Available operators: +, -, * and /")
     sys.exit(1)
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
+# Cast the arguments to integers
+try:
+    a = int(a)
+    b = int(b)
+except ValueError:
+    print("Invalid argument. Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
+# Perform the operation
+if operator == "+":
+    result = add(a, b)
+elif operator == "-":
+    result = sub(a, b)
+elif operator == "*":
+    result = mul(a, b)
+elif operator == "/":
+    result = div(a, b)
+
+# Print the result
 print("{} {} {} = {}".format(a, operator, b, result))
