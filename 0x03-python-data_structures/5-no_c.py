99b0ee816de56bfc28c0a33598704adb38e4aa4d
lwafulah <loice.lovie@gmail.com>
2023-05-15 13:49:28 -0700

    remove character

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    remove = ['c', 'C']
-    new_string = ''.join(i for i in my_string if not i in remove)
+    char_list = list(my_string)
+
+    for char in ['c', 'C']:
+        while char in char_list:
+            char_list.remove(char)
+
+    new_string = ''.join(char_list)
+
     return new_string
