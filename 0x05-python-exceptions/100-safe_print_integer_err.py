83fe654c3fe8fa990e998294b388b6ca332e7419
lwafulah <loice.lovie@gmail.com>
2023-05-29 08:03:19 -0700

    print and integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+
 import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        value = int(value)  # Convert the value to an integer
+        print(value)
         return True
+    except ValueError:
+        print("Exception: Invalid integer value", file=sys.stderr)
+        return False
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
