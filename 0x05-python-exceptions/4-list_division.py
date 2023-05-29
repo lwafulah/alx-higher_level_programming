12c77118235c1a4feb1a73f0b59b9dbf3b1f5569
lwafulah <loice.lovie@gmail.com>
2023-05-29 07:49:34 -0700

    Divide element in 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = 0
+            if i < len(my_list_1) and i < len(my_list_2):
+                if my_list_2[i] == 0:
+                    raise ZeroDivisionError
+                result = my_list_1[i] / my_list_2[i]
+            elif i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        except (TypeError, ValueError):
+            print("wrong type")
+        finally:
+            new_list.append(result)
+    return new_list
