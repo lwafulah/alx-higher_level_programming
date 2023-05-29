0f5eda76399486448fe205bc710e90193ad94097
lwafulah <loice.lovie@gmail.com>
2023-05-29 05:27:52 -0700

    Updated

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-    if b < 0:
-        a = 1 / a
-        b = -b
-    while b:
-        if b & 1:
-            result *= a
-        a *= a
-        b >>= 1
-    return result
+    return a ** b
