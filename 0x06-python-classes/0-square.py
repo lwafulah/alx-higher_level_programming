b63f85729ef0d181ea8d23ef5e51d73dbfb7331d
lwafulah <loice.lovie@gmail.com>
2023-05-30 12:30:18 -0700

    Class Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
-
+"""Definition of class square"""
 class Square:
-    """A class that defines the properties and methods of a square."""
-
-    def __init__(self):
-        """Constructor of the class that initializes the side_length of the square to None."""
-        self.__side_length = None
-
-    def get_side_length(self):
-        """Returns the side_length of the square."""
-        return self.__side_length
-
-    def set_side_length(self, side_length):
-        """Sets the side_length of the square."""
-        self.__side_length = side_length
+    """Represent a square"""
 
     pass
