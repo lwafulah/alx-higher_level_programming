c752db8c0ce5de2a5268d531429020edf260f060
lwafulah <loice.lovie@gmail.com>
2023-05-30 12:41:17 -0700

    Class Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 """Definition of class square"""
 class Square:
-    """Represent a square
-
-    Attributes:
-    Perimeter: return the perimeter of the square
-    Area: Return the ares of the square
-
-    """
+    """Represent a square"""
     pass
