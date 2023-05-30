8dbc162d558c15400e4f04c0ce78e43a37b405b5
lwafulah <loice.lovie@gmail.com>
2023-05-30 12:44:46 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """A Square class that defines a square with size
+
+    Attributes:
+        _size (int): the size of the square
+    """
+
+    def __init__(self, size):
+        """Initialize a Square object
+
+        Args:
+            size (int): The size of the square.
+        """
+        self._size = size
