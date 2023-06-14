7b0d7b32f554cd8c6e14e859a9492e3eb5148b7a
lwafulah <loice.lovie@gmail.com>
2023-06-14 11:22:11 -0700

    Read File

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF-8) and prints its contents to stdout.
+
+    Args:
+        filename (str, optional): The name of the file to be read. If not provided, an empty string is used as the default value.
+
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
+
