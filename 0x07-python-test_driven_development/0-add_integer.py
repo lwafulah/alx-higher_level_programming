283ee004dde3d9fe496e64ccab843a6efbdd4b4c
lwafulah <loice.lovie@gmail.com>
2023-06-22 01:24:28 -0700

    Addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,7 +18,7 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, float):
         raise TypeError("a must be an integer")
-    not isinstance(b, float):
+    if not isinstance(b, float):
         raise TypeError("b must be an integer")
 
         a = int(a)
